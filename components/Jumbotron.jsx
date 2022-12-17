d2d81e2a355f399312f381b49110199ab7720f4d
Davey Mason <daveymason@outlook.com>
2022-12-17 22:54:24 +0100

    newbg

--- components/Jumbotron.jsx
+++ components/Jumbotron.jsx
@@ -1,5 +1,5 @@
 import Header from '@components/Header'
-import { Box, Typography } from '@mui/material';
+import { Box, Typography, Divider } from '@mui/material';
 
 export default function Jumbotron() {
   return (
@@ -14,10 +14,11 @@ export default function Jumbotron() {
       boxShadow="2px 2px 4px #282828"
       borderRadius="10px"
     >
-      <Typography variant="h1" component="h1">
+      <Typography variant="h1" component="h1" sx={{mb:5, color: '#734b2e'}}>
         Kangal
       </Typography>
-      <Typography variant="subtitle1">
+      <Divider />
+      <Typography variant="subtitle1" sx={{p:3}}>
       The Kangal Dog is a large, brave and loyal breed originating from Turkey. These majestic dogs are known for their protective nature and have been used for centuries to guard sheep and other livestock. They are highly intelligent and easily trained, making them excellent working dogs. This website is dedicated to providing information about Kangal Dogs, including their history, physical characteristics, and unique personality traits. Whether you are looking for a loyal guardian for your property or a loyal companion for your family, the Kangal Dog may be the perfect fit.
       </Typography>
     </Box>
